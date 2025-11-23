deeb9daf91397d8f7bca4ee8497f53a934c65e15
Deepak Mallareddy <deepak8519m@gmail.com>
2025-11-23 22:38:08 +0530

    code added

--- DAY3/ten-days-of-voice-agents-2025-main/frontend/components/app/welcome-view.tsx
+++ DAY3/ten-days-of-voice-agents-2025-main/frontend/components/app/welcome-view.tsx
@@ -34,7 +34,7 @@ export const WelcomeView = ({
         <WelcomeImage />
 
         <p className="text-foreground max-w-prose pt-1 leading-6 font-medium">
-          Chat live with your voice AI agent
+          Chat live with your Health and Wellness–Oriented Voice Agent
         </p>
 
         <Button variant="primary" size="lg" onClick={onStartCall} className="mt-6 w-64 font-mono">
@@ -44,7 +44,8 @@ export const WelcomeView = ({
 
       <div className="fixed bottom-5 left-0 flex w-full items-center justify-center">
         <p className="text-muted-foreground max-w-prose pt-1 text-xs leading-5 font-normal text-pretty md:text-sm">
-          Need help getting set up? Check out the{' '}
+          Each day, the agent checks in with the user about their mood and goals, has a short
+          conversation, and stores the results in a JSON file so it can refer back to previous days.{' '}
           <a
             target="_blank"
             rel="noopener noreferrer"
